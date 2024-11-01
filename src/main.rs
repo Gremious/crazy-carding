39add3801b55517352d32dcf66f021ce329f26c8
Gremious Grenald <gremious@gremy.co.uk>
2024-11-01 14:31:57 +0000

    Get random mtg card

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::LazyLock;
+
+pub static CLIENT: LazyLock<reqwest::Client> = LazyLock::new(reqwest::Client::new);
+
+pub mod skryfall;
+use skryfall as mtg;
+
+#[tokio::main]
+async fn main() {
+	let mtg_card = mtg::get_random_card().await.unwrap();
+	print_img(&mtg_card);
+}
+
+pub fn print_img(img: &image::DynamicImage) {
+	use viuer::{print, Config};
+
+	let conf = Config {
+		// Start from row 4 and column 20.
+		height: Some(30),
+		..Default::default()
+	};
+
+	print(img, &conf).expect("Image printing failed.");
 }
---
+++ src/skryfall.rs
@@ -0,0 +1,16 @@
+use super::*;
+
+pub async fn get_random_card() -> anyhow::Result<image::DynamicImage> {
+	let bytes = CLIENT.get("https://api.scryfall.com/cards/random")
+		.query(&[
+			("format", "image"),
+			("version", "png"),
+		])
+		.header(reqwest::header::USER_AGENT, "yank-images")
+		.header(reqwest::header::ACCEPT, "*/*")
+		.send().await?
+		.error_for_status()?
+		.bytes().await?;
+
+	Ok(image::load_from_memory(&bytes)?)
+}
