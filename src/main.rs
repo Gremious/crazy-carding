4384344f1eeb359506ef43b93178b67dc76acf53
Gremious Grenald <gremious@gremy.co.uk>
2025-01-13 03:22:15 +0000

    WIP working font rendering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::LazyLock;
+use std::{collections::VecDeque, sync::LazyLock};
+use itertools::Itertools;
 
 pub static CLIENT: LazyLock<reqwest::Client> = LazyLock::new(reqwest::Client::new);
 
@@ -7,7 +8,10 @@ pub mod skryfall;
 pub mod ygoprodeck;
 
 use hearthstone as hs;
+use image::{DynamicImage, ExtendedColorType};
+use rand::Rng;
 use skryfall as mtg;
+use tap::{Pipe, Tap};
 use tokio::sync::Mutex;
 use ygoprodeck as ygo;
 
@@ -49,15 +53,43 @@ impl State {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
 	init().await.unwrap();
+	// let mut rng = rand::thread_rng();
 
-	// let mtg_card = mtg::get_random_card().await.unwrap();
-	// print_img(&mtg_card);
-	// let ygo_card = ygo::get_random_card().await.unwrap();
-	// print_img(&ygo_card);
-	let hs_card = hs::get_random_card().await.unwrap();
-	print_img(&hs_card);
+	std::fs::create_dir_all("./images")?;
+
+	// let card_img = mtg::get_random_card().await?;
+
+	// let len = card_img.as_bytes().len();
+	// log::info!("pre-resize len: {}", len);
+	// let width = card_img.width();
+	// let height = card_img.height();
+
+	// let card_img = match rng.gen_range(0..2) {
+		// 0 => mtg::get_random_card().await?,
+		// 1 => ygo::get_random_card().await?,
+		// 2 => hs::get_random_card().await?,
+		// _ => unreachable!(),
+	// };
+
+	// log::info!("original: {}x{}", card_img.width(), card_img.height());
+	// let card_img = ygo::resize_card_image(card_img)?;
+	// log::info!("resized: {}x{}", card_img.width(), card_img.height());
+
+	// let dt = chrono::Local::now().format("%+");
+	// let name = format!("images/{dt}.png");
+	// card_img.save(&name)?;
+	// print_card(&name, ygo::get_ppi(&card_img));
+	// print_img(&card_img);
+
+	// let text = text_to_png("Haste ", "(This creature can attack and {T} as soon as it comes under your control.)");
+	// let text = text_to_png("Áaaa ", "Áaaa Aaaa Áaaa Aaaa Aaaa Áaaa Aaaa Áaaa Aaaa Áaaa Aaaa Áaaa Aaaa Áaaa");
+	// let text = text_to_png("Áaaa ", "Áaaa Aaaa Áaaa Aaaa");
+	// text.save("images/text.png")?;
+	txt_img().save("images/txt.png")?;
+
+	Ok(())
 }
 
 async fn init() -> anyhow::Result<()> {
@@ -80,7 +112,7 @@ async fn init() -> anyhow::Result<()> {
 	Ok(())
 }
 
-pub fn print_img(img: &image::DynamicImage) {
+pub fn display_img(img: &image::DynamicImage) {
 	use viuer::{print, Config};
 
 	let conf = Config {
@@ -89,4 +121,179 @@ pub fn print_img(img: &image::DynamicImage) {
 	};
 
 	print(img, &conf).expect("Image printing failed.");
+}
+
+pub fn print_card(path: &str, ppi: u32) {
+	// lp -o media=3x5,ByPassTray,Stationery -o ColorMode=Gray -o ppi=300 ./images/latest.jpg
+	let cmd = std::process::Command::new("lp")
+		.args(&[
+			"-o",
+			"media=3x5,ByPassTray,Stationery",
+			"-o",
+			"ColorMode=Gray",
+			"-o",
+			&format!("ppi={ppi}"),
+			path,
+		])
+		.output()
+		.expect("Failed to execute command.");
+
+	log::info!("{:?}", cmd);
+}
+
+pub fn cat_print(text: &str) {
+}
+
+pub fn txt_img() -> DynamicImage {
+	use imageproc::drawing::draw_text_mut;
+
+	let mplantin = include_bytes!("../resources/fonts/mplantin.ttf");
+	let mplantin = ab_glyph::FontArc::try_from_slice(mplantin).unwrap();
+
+	let font_size = 128.0;
+	let word = "ÁÄaaa";
+
+	let (mut w, mut h) = imageproc::drawing::text_size(font_size, &mplantin, word);
+	let mut image: DynamicImage = DynamicImage::new_rgba8(w, h);
+
+	draw_text_mut(&mut image, image::Rgba([ 100u8, 100u8, 100u8, 255u8 ]), 0, 16, font_size, &mplantin, word);
+
+	image
+}
+
+// We're doing all this to print with italic and bold text and maybe even symbols (wow)
+// on the thermal printer
+pub fn text_to_png(keyword: &str, explanation: &str) -> DynamicImage {
+	use imageproc::drawing::draw_text_mut;
+
+	let mplantin = include_bytes!("../resources/fonts/mplantin.ttf");
+	let mplantin = ab_glyph::FontArc::try_from_slice(mplantin).unwrap();
+
+	let mplantin_it = include_bytes!("../resources/fonts/mplantin-italic.ttf");
+	let mplantin_it = ab_glyph::FontArc::try_from_slice(mplantin_it).unwrap();
+
+	let mplantin_bold = include_bytes!("../resources/fonts/mplantin-bold.ttf");
+	let mplantin_bold = ab_glyph::FontArc::try_from_slice(mplantin_bold).unwrap();
+
+	let font_size = 32.0;
+	let max_width_px = 625;
+
+
+	// (Text line, width, height)
+	let mut lines: Vec<(String, u32, u32)> = Vec::new();
+
+	let (w_kw, h_kw) = imageproc::drawing::text_size(font_size, &mplantin_bold, keyword);
+	let (mut w_expl, mut h_expl) = imageproc::drawing::text_size(font_size, &mplantin_it, explanation);
+
+	// Splits words to fit, like this:
+	//
+	// (This creature can attack and {T} as soon as it comes under your control.)
+	// ->
+	// (This creature can attack  |
+	// and {T} as soon as it comes under your control.)
+	// ->
+	// (This creature can attack  |
+	// and {T} as soon as it comes|
+	// under your control.)       |
+	fn split_text<'a>(
+		w_kw: u32,
+		w_expl: u32,
+		h_expl: u32,
+		max_width_px: u32,
+		mut curr_line_words: Vec<&'a str>,
+		leftover: &mut VecDeque<&'a str>,
+		font_size: f32,
+		mplantin_it: &ab_glyph::FontArc,
+		lines: &mut Vec<(String, u32, u32)>,
+	) {
+		if w_kw + w_expl > max_width_px {
+			let pop = curr_line_words.pop().unwrap();
+			leftover.push_front(pop);
+
+			let new_curr_line = curr_line_words.iter().map(|x| x.to_owned()).intersperse(" ").collect::<String>();
+			let (new_w, new_h) = imageproc::drawing::text_size(font_size, mplantin_it, &new_curr_line);
+
+			split_text(w_kw, new_w, new_h, max_width_px, curr_line_words, leftover, font_size, mplantin_it, lines);
+		} else {
+			let new_curr_line = curr_line_words.iter().map(|x| x.to_owned()).intersperse(" ").collect::<String>();
+			lines.push((new_curr_line, w_expl, h_expl));
+
+			let leftovers = leftover.iter().map(|x| x.to_owned()).intersperse(" ").collect::<String>();
+			let (rest_w, rest_h) = imageproc::drawing::text_size(font_size, mplantin_it, &leftovers);
+
+			if leftover.is_empty() {
+				return;
+			}
+
+			split_text(
+				0,
+				rest_w,
+				rest_h,
+				max_width_px,
+				leftover.iter().map(|x| *x).collect::<Vec<_>>(),
+				&mut VecDeque::new(),
+				font_size,
+				mplantin_it,
+				lines,
+			);
+
+		}
+	}
+
+	if w_kw + w_expl > max_width_px {
+		let mut curr_line_words = explanation.split_whitespace().collect::<Vec<_>>();
+		let mut leftover = VecDeque::new();
+
+		let pop = curr_line_words.pop().unwrap();
+		leftover.push_front(pop);
+
+		let new_curr_line = curr_line_words.iter().map(|x| x.to_owned()).intersperse(" ").collect::<String>();
+		let (new_w, new_h) = imageproc::drawing::text_size(font_size, &mplantin_it, &new_curr_line);
+
+		split_text(w_kw, new_w, new_h, max_width_px, curr_line_words, &mut leftover, font_size, &mplantin_it, &mut lines);
+
+	} else {
+		lines.push((explanation.to_owned(), w_expl, h_expl));
+	}
+
+
+	log::info!("{}x{}", w_kw + w_expl, h_kw);
+
+	let height = if lines.len() == 1 {
+		h_kw.max(h_expl)
+	} else {
+		let (_, _, h_first_line) = lines.first().unwrap();
+		let first_height = h_kw.max(*h_first_line);
+		let rest = lines.iter().skip(1).map(|(_, _, h)| h).sum::<u32>();
+		first_height + rest
+	};
+
+	let mut image: DynamicImage = DynamicImage::new_rgba8(max_width_px, height);
+
+	if lines.len() == 1 {
+		draw_text_mut(&mut image, image::Rgba([ 0u8, 0u8, 0u8, 255u8 ]), 0, 0, font_size, &mplantin_bold, keyword);
+		let (explanation, _, _) = &lines[0];
+		draw_text_mut(&mut image, image::Rgba([ 0u8, 0u8, 0u8, 255u8 ]), w_kw as i32, 0, font_size, &mplantin, &explanation);
+	} else {
+		draw_text_mut(&mut image, image::Rgba([ 0u8, 0u8, 0u8, 255u8 ]), 0, 0, font_size, &mplantin, keyword);
+		let (explanation, _, h) = &lines[0];
+		draw_text_mut(&mut image, image::Rgba([ 0u8, 0u8, 0u8, 255u8 ]), w_kw as i32, 0, font_size, &mplantin, &explanation);
+
+		let mut prev_h = h_kw.max(*h);
+
+		for (line, _, h) in lines.iter().skip(1) {
+			draw_text_mut(
+				&mut image,
+				image::Rgba([ 0u8, 0u8, 0u8, 255u8 ]),
+				0,
+				prev_h as i32,
+				font_size,
+				&mplantin,
+				&line,
+			);
+			prev_h += h;
+		}
+	}
+
+	image
 }
